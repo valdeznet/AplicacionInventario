b6fe25352e3f46272c21216e2194646c4fba43af
Pablo Valdez <valdeznet@gmail.com>
2022-10-19 09:15:54 -0300

    actualiz 18/10

---
+++ AplicacionInventario/AplicacionInventario/Date.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AplicacionInventario
+{
+    public class Date
+    {
+        private int _year;
+        private int _month;
+        private int _day;
+
+        public Date(int year, int month, int day)
+        {
+            _year = year;
+            _month = CheckMonth(month);
+            _day = day;
+        }
+
+        private int CheckMonth(int month)
+        {
+            if (month>=1 && month<=12)
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ AplicacionInventario/AplicacionInventario/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace AplicacionInventario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("AplicacionInventario");
+            Console.WriteLine("====================");
+            var date1 = new
+        }
+    }
+}
